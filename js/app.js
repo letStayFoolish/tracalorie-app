50fe556e69eb5f55449ee9c4e808989b8d2be31c
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-09 12:23:39 +0700

    Refactor to Single _newItem Method

--- js/app.js
+++ js/app.js
@@ -111,57 +111,36 @@ class App {
 
     document
       .getElementById('meal-form')
-      .addEventListener('submit', this._newMeal.bind(this));
+      .addEventListener('submit', this._newItem.bind(this, 'meal'));
     document
       .getElementById('workout-form')
-      .addEventListener('submit', this._newWorkout.bind(this));
+      .addEventListener('submit', this._newItem.bind(this, 'workout'));
   }
 
-  _newMeal(e) {
+  _newItem(type, e) {
     e.preventDefault();
 
-    const mealName = document.getElementById('meal-name');
-    const mealCalories = document.getElementById('meal-calories');
+    const name = document.getElementById(`${type}-name`);
+    const calories = document.getElementById(`${type}-calories`);
 
     // Validate Inputs
 
-    if (mealName.value === '' || mealCalories.value === '') {
+    if (name.value === '' || calories.value === '') {
       alert('Please fill in all fields');
       return;
     }
-
-    const meal = new Meal(mealName.value, +mealCalories.value);
-    this._tracker.addMeal(meal);
-    mealName.value = '';
-    mealCalories.value = '';
-
-    const collapseMeal = document.getElementById('collapse-meal');
-    const bsCollapse = new bootstrap.Collapse(collapseMeal, { toggle: true });
-  }
-
-  _newWorkout(e) {
-    e.preventDefault();
-
-    const workoutType = document.getElementById('workout-name');
-    const workoutBurnedCalories = document.getElementById('workout-calories');
-
-    // Validate Inputs
-
-    if (workoutType.value === '' || workoutBurnedCalories.value === '') {
-      alert('Please fill in all fields');
-      return;
+    if (type === 'meal') {
+      const meal = new Meal(name.value, +calories.value);
+      this._tracker.addMeal(meal);
+    } else if (type === 'workout') {
+      const workout = new Workout(name.value, +calories.value);
+      this._tracker.addWorkout(workout);
     }
+    name.value = '';
+    calories.value = '';
 
-    const workout = new Workout(
-      workoutType.value,
-      +workoutBurnedCalories.value
-    );
-    this._tracker.addWorkout(workout);
-    workoutType.value = '';
-    workoutBurnedCalories.value = '';
-
-    const collapseWorkout = document.getElementById('collapse-workout');
-    const bsCollapse = new bootstrap.Collapse(collapseWorkout, {
+    const collapseItem = document.getElementById(`collapse-${type}`);
+    const bsCollapse = new bootstrap.Collapse(collapseItem, {
       toggle: true,
     });
   }
