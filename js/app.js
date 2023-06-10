ed9c15d65c0a9091392b24d5e97d26a479f49fbf
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-10 13:18:52 +0700

    Save Meals To Local Storage

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ class CalorieTracker {
   constructor() {
     this._calorieLimit = Storage.getCalorieLimit();
     this._totalCalories = Storage.getTotalCalories(0);
-    this._meals = [];
+    this._meals = Storage.getMeals();
     this._workouts = [];
     this._displayCaloriesLimit();
     this._displayCaloriesTotal();
@@ -16,6 +16,7 @@ class CalorieTracker {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
     Storage.updateTotalCalories(this._totalCalories);
+    Storage.saveMeal(meal);
     this._displayNewMeal(meal);
     this._render();
   }
@@ -62,6 +63,10 @@ class CalorieTracker {
     this._displayCaloriesLimit();
     Storage.setCalorieLimit(calorieLimit);
     this._render();
+  }
+
+  loadItems() {
+    this._meals.forEach((meal) => this._displayNewMeal(meal));
   }
 
   // Private Methods
@@ -213,7 +218,7 @@ class Storage {
     if (localStorage.getItem('calorieTotal') === null) {
       calorieTotal = defaultCalories;
     } else {
-      calorieTotal = localStorage.getItem('calorieTotal');
+      calorieTotal = +localStorage.getItem('calorieTotal');
     }
     return calorieTotal;
   }
@@ -221,12 +226,34 @@ class Storage {
   static updateTotalCalories(calories) {
     localStorage.setItem('calorieTotal', calories);
   }
+
+  static getMeals() {
+    let meals;
+
+    if (localStorage.getItem('meals') === null) {
+      meals = [];
+    } else {
+      meals = JSON.parse(localStorage.getItem('meals'));
+    }
+
+    return meals;
+  }
+
+  static saveMeal(meal) {
+    const meals = Storage.getMeals();
+    meals.push(meal);
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
 }
 
 class App {
   constructor() {
     this._tracker = new CalorieTracker();
+    this._loadEventListeners();
+    this._tracker.loadItems();
+  }
 
+  _loadEventListeners() {
     document
       .getElementById('meal-form')
       .addEventListener('submit', this._newItem.bind(this, 'meal'));
