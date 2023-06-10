299d9b46625276f72a2ecafd7b63b2e53b745a66
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-10 14:11:39 +0700

    Remove Meals & Workouts From LocalStorage

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ class CalorieTracker {
       this._totalCalories -= meal.calories;
       Storage.updateTotalCalories(this._totalCalories);
       this._meals.splice(index, 1);
+      Storage.removeMeal(id);
       this._render();
     }
   }
@@ -48,6 +49,7 @@ class CalorieTracker {
       this._totalCalories += workout.calories;
       Storage.updateTotalCalories(this._totalCalories);
       this._workouts.splice(index, 1);
+      Storage.removeWorkout(id);
       this._render();
     }
   }
@@ -247,6 +249,17 @@ class Storage {
     localStorage.setItem('meals', JSON.stringify(meals));
   }
 
+  static removeMeal(id) {
+    const meals = Storage.getMeals();
+
+    meals.forEach((meal, index) => {
+      if (meal._id === id) {
+        meals.splice(index, 1);
+      }
+    });
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
+
   static getWorkouts() {
     let workouts;
     if (localStorage.getItem('workouts') === null) {
@@ -263,6 +276,17 @@ class Storage {
     const workouts = Storage.getWorkouts();
     workouts.push(workout);
 
+    localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+
+  static removeWorkout(id) {
+    const workouts = Storage.getWorkouts();
+
+    workouts.forEach((workout, index) => {
+      if (workout._id === id) {
+        workouts.splice(index, 1);
+      }
+    });
     localStorage.setItem('workouts', JSON.stringify(workouts));
   }
 }
